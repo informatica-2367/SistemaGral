c622fd53e3e76443dd37c20b6dd91b88d44204e7
m4jbz <marcoajb05@proton.me>
2024-12-17 23:42:55 -0600

    clientes route fue modificada

--- app/Modules/Clientes/Routes/web.php
+++ app/Modules/Clientes/Routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Modules\Clientes\Controllers\ClienteController;
 
 Route::get('/clientes', [ClienteController::class, 'index'])->name('cliente.index');
-Route::get('cliente/create', [ClienteController::class, 'create'])->name('cliente.create');
+Route::get('/cliente/create', [ClienteController::class, 'create'])->name('cliente.create');
 Route::post('/clientes', [ClienteController::class, 'store'])->name('cliente.store');
 Route::delete('/clientes/{id}', [ClienteController::class, 'delete'])->name('cliente.delete');
 Route::get('/clientes/{id}/edit', [ClienteController::class, 'edit'])->name('cliente.edit');
